e85a99e947142030bf05282caae9142f1f6abaa2
Thiago Assis <55933312+thiagoassisk8@users.noreply.github.com>
2020-05-18 14:48:08 -0300

    Update dados.sql

--- banco_dados/dados.sql
+++ banco_dados/dados.sql
@@ -3,18 +3,18 @@ USE db_empresa;
 
 -- Presidente:
 insert tb_funcionarios_empresa (identidade, nome, sobrenome, dt_nascimento, dt_contratacao, departamento, cargo, salario) 
-values (1, 'Thiago', 'Assis', '1999-03-23', '2011-04-15', 10, 'Presidente', '400000');
+values (1, 'Thiago', 'Assis', '1999-03-23', '2011-04-15', 10, 'Fundador', '400000');
 
 -- Diretores Gerais:
 insert tb_funcionarios_empresa (identidade, nome, sobrenome, dt_nascimento, dt_contratacao, departamento, cargo, salario) 
-values (2, 'Isabela', 'Saraiva', '2000-02-16', '2011-05-16', 11, 'Diretor', '350000');
+values (2, 'Isabela', 'Saraiva', '2000-02-16', '2011-05-16', 10, 'Fundador', '350000');
 
 insert tb_funcionarios_empresa (identidade, nome, sobrenome, dt_nascimento, dt_contratacao, departamento, cargo, salario) 
-values (3, 'Gabriel', 'Caldas', '1997-10-12', '2011-05-16', 11, 'Diretor', '350000');
+values (3, 'Gabriel', 'Caldas', '1997-10-12', '2011-05-16', 1, 'Fundador', '350000');
 
 -- Desenvolvedores:
 insert into tb_funcionarios_empresa (identidade, nome, sobrenome, dt_nascimento, dt_contratacao, cod_gerente, cargo, departamento, salario) 
-values (1234, 'Caue', 'Moura', '1987-11-11', '2010-04-18', 1, 'Desenvolvedor', 15,'40000');
+values (1234, 'Caue', 'Moura', '1987-11-11', '2010-04-18', 1, 'Desenvolvedor', 15,'75000');
 
 insert into tb_funcionarios_empresa (identidade, nome, sobrenome, dt_nascimento, dt_contratacao, cod_gerente, cargo, departamento, salario) 
 values (3456, 'Whindersson', 'Nunes', '1995-01-05', '2012-02-25', 1, 'Desenvolvedor', 15, '40000');
@@ -30,7 +30,7 @@ insert into tb_funcionarios_empresa (identidade, nome, sobrenome, dt_nascimento,
 values (1010, 'Júlio', 'Cocielo', '1993-05-25', '2015-01-10', 6, 'Engenheiro', 14, '125000');
 
 insert into tb_funcionarios_empresa (identidade, nome, sobrenome, dt_nascimento, dt_contratacao, cod_gerente, cargo, departamento, salario) 
-values (1011, 'Jõao Carlos', 'Azevedo', '1995-07-20', '2013-12-24', 6, 'Engenheiro', 14, '125000');
+values (1011, 'João Carlos', 'Azevedo', '1995-07-20', '2013-12-24', 6, 'Engenheiro', 14, '125000');
 
 insert into tb_funcionarios_empresa (identidade, nome, sobrenome, dt_nascimento, dt_contratacao, cod_gerente, cargo, departamento, salario) 
 values (1012, 'João Pedro', 'Pinto', '1987-11-04', '2017-06-18', 6, 'Engenheiro', 14, '125000');
@@ -73,7 +73,7 @@ values (6468, 'João Davi', 'Ferreira', '1999-03-24', '2011-04-17', 40, 'Analist
     insert into tb_outras_empresas(cod_empresa,nme_empresa,cnpj)
     values(4570,'PassooPano','0703782');
     
-    insert into tb_outras_empresas(cod_empresa,nme_empresa,cnpj)
+    replace into tb_outras_empresas(cod_empresa,nme_empresa,cnpj)
     values(4571,'CantinaDi','0403751');
 
 -- Funcionarios de outra empresa:
