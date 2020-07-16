649b5a80eaa3ad6a038785a956a6bbb1b80f3031
Thiago Assis <55933312+thiagoassisk8@users.noreply.github.com>
2020-07-16 15:50:49 -0300

    [update] -- Atualização de scritps

--- Jogo_forca 2.0/index.js
+++ Jogo_forca 2.0/index.js
@@ -1,73 +1,129 @@
-let letras_disponiveis  = ["a","b","c","d","e","f","g",
-"h","i","j","k","l","m","n","o","p","q","r","s","t","u",
-"v","w","x","y","z","ç","ã","é"];
-
 let esportes = ["futebol","skate","basquete","natação"];
 let youtubers = ["Felipe Neto","Lucas Lira", "Cocielo","Cauê Moura","PC Siqueira"];
 let animais = ["Cachorro","Leão","Urso"];
 let objetos = ["Mesa","celular","peruca"];
+//let wrongletters = [];
 
+//Soreteio da categoria/palavra secreta
+let categorias = [esportes,youtubers,animais,objetos];
+categoriaSorteada = categorias[Math.floor(Math.random() * categorias.length)];
+
+secretword = categoriaSorteada[Math.floor(Math.random() * categoriaSorteada.length)];
+//secretword = "";
+
+function barrinha(){
+  let quant = secretword.length;
+  let barrinhas = ({ toString: () => " _ ",repeat: String.prototype.repeat}).repeat(quant);//converte a qntd de letras em "_"
+  document.getElementById("palavraSecreta").innerHTML=barrinhas
+}
+
+function del_ltrs_digitds(){
+  let wrongletters = document.getElementById("erradasletras");
+  while (wrongletters.hasChildNodes()){
+    wrongletters.removeChild(wrongletters.firstChild);
+  }
+  //barrinha();
+}
 
 //Menu principal
 function funcao_esportes(){
-  let secretword = esportes[Math.floor(Math.random() * esportes.length)];
-  let quant = secretword.length;//define a quantidade de letras da palavra secreta
-  let barrinhas = ({ toString: () => " _ ",repeat: String.prototype.repeat}).repeat(quant);//converte a qntd de letras em "_"
-  document.getElementById("palavraSecreta").innerHTML=barrinhas;
-  document.getElementById("palavraSecreta").value=secretword;
-  //barrinhas =   
-  //return secretword
+  window.secretword = esportes[Math.floor(Math.random() * esportes.length)];
+  barrinha()  
+  document.getElementById("palavraSecreta").value=window.secretword;
+  del_ltrs_digitds()
 }
 
 function funcao_youtubers(){
-  let secretword = youtubers[Math.floor(Math.random()*youtubers.length)]
-  let quant = secretword.length;
-  let barrinhas = ({ toString: () => " _ ",repeat: String.prototype.repeat}).repeat(quant);
-  document.getElementById("palavraSecreta").innerHTML=barrinhas;
-  document.getElementById("bt_ytbers").value=secretword;
+  window.secretword = youtubers[Math.floor(Math.random()*youtubers.length)]
+  barrinha()
+  document.getElementById("palavraSecreta").value=window.secretword;
+  del_ltrs_digitds()
   return secretword;
   
 }
 
 function funcao_animais(){
-  let secretword = animais[Math.floor(Math.random()*animais.length)]
-  let quant = secretword.length;
-  let barrinhas = ({ toString: () => " _ ",repeat: String.prototype.repeat}).repeat(quant);
-  document.getElementById("palavraSecreta").innerHTML=barrinhas;
+  window.secretword = animais[Math.floor(Math.random()*animais.length)]
+  barrinha()
   document.getElementById("bt_animais").value = secretword;
+  del_ltrs_digitds()
   return secretword;
 }
 function funcao_objetos(){
-  let secretword = objetos[Math.floor(Math.random()*objetos.length)]
-  let quant = secretword.length;
-  let barrinhas = ({ toString: () => ' _ ', repeat: String.prototype.repeat }).repeat(quant);
-  document.getElementById("bt_objetos").value = secretword;
-  document.getElementById("palavraSecreta").innerHTML = barrinhas;
+  window.secretword = objetos[Math.floor(Math.random()*objetos.length)]
+  barrinha()
+  document.getElementById("bt_objetos").value = secretword; 
+  del_ltrs_digitds()
   return secretword;
 }
 
 //Letra (input/usuário)
 function funcao_letra(){
-   document.getElementById("letra").value;  
-  
-    
+  //let letter = document.getElementById("letra").value;
+  //console.log("funcionou", letter)
+  //letter.addEventListener("Keyup",function(event){
+    //if(event.Keycode == 13){
+    //  event.preventDefault();
+    //  document.getElementById("bt_ok").click();
+    //}
+  //});
+
 }
   
-  function funcao_ok(){
-    let letter = funcao_letra();
-    //alert(letter)
-    secretword = document.getElementById("palavraSecreta").value;
-    console.log(secretword,"Letra digitada:",letter);
-    return letter
-    
-  
-}  
+function funcao_ok(){  
+  let letter = document.getElementById("letra").value;
+  let t = document.createTextNode(letter);
+  let wrongletters = document.createElement("li");
+  wrongletters.className = "NotIn"
+  console.log(secretword);
+  for(let i=0; i < secretword.length;i++ ){
+    if(letter == secretword[i]){    
+     console.log("letra certa",i)
+     console.log("letra certa",secretword[i])
+     
+     
+     
+     
+      
+   }
+   else if (letter != secretword[i]) {
+
+    wrongletters.append(t);
+    document.getElementById("erradasletras").appendChild(wrongletters);
+      
+
+     }
+    //console.log("letra errada",letter);
+    //console.log("valor de i:",secretword[i])
+    else {
+     
+   }{              
+     //wrongletter=document.createElement("NotIn")
+     //document.getElementById("letrasErradas").appendChild(wrongletter);                          
+
+    }    
+  }
+}
+
 function funcao_restart(){
+  
   console.log("restart");
 
 }
-
+//",,,
 function funcao_dica(){
-  
+  if (secretword =="futebol"){
+    
+  }
+  if(secretword == "skate"){
+
+  }
+  if(secretword =="basquete"){
+    
+  }
+  if(secretword=="natação"){
+
+  }
 
 }
+barrinha();
